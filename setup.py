cddc28463cdd8f730355c0e195c16ab12cd53506
nrser <neil@neilsouza.com>
2021-07-07 21:26:17 -0400

    Here be v0.1.1

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", "r") as fh:
 setuptools.setup(
     name="nansi",
     # SEE   https://www.python.org/dev/peps/pep-0440/
-    version="0.1.1a0",
+    version="0.1.1",
     author="nrser",
     author_email="neil@neilsouza.com",
     description="Nrser ANSIble",
